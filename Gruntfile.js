274d7196121e51b80da17d5fb3334a18533ac7fb
trinity <imandry.c@gmail.com>
2014-03-28 11:08:46 +0800

    Framework: from grunt-f2e-boilerplate
    JavaScript
    SASS/SCSS AutoPrefixer
    Jade

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,100 +1,138 @@
 
+var _ = require('lodash');
+var path = require('path');
+
 module.exports = function(grunt) {
+
+  var BUILD_ROOT = 'build/';
+
+  // Files path definition {{{
+
+  var files = function (cwd, src, dest, ext) {
+
+    var file = {
+      path: path.join(cwd, src),
+      pattern: {
+        expand: true,
+      }
+    };
+
+    if (cwd) file.pattern.cwd = cwd;
+    if (src) file.pattern.src = src;
+    if (dest) file.pattern.dest = dest;
+    if (ext) file.pattern.ext = ext;
+
+    return file;
+  };
+
+  _.extend(files, {
+
+    script    : files('src/'  , 'script/**/*.js'  , BUILD_ROOT, '.js'),
+    document  : files('src/'  , '**/*.jade'       , BUILD_ROOT, '.html'),
+    style     : files('src/'  , 'style/**/*.scss' , BUILD_ROOT, '.css'),
+    asset     : files('asset/', '**/*'            , BUILD_ROOT),
+
+  });
+
+  // }}}
 
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
 
     jshint: {
-      all: [ 'Gruntfile.js', 'src/script/**/*.js' ]
+      gruntfile : [ 'Gruntfile.js' ],
+      script    : [ files.script.path ],
     },
 
     jade: {
-      compile: {
-        options: {
-          pretty: true
-        },
-        expand: true,
-        cwd: 'src/',
-        dest: 'build/',
-        src: '**/*.jade',
-        ext: '.html'
+      document: files.document.pattern,
+      options: {
+        pretty: true
       }
     },
 
-    stylus: {
-      compile: {
-        options: {
-          use: [
-            require('axis-css'),
-            require('nib'),
-            require('rupture'),
-          ],
-        },
-        expand: true,
-        cwd: 'src/',
-        dest: 'build/',
-        src: 'style/**/*.styl',
-        ext: '.css'
+    sass: {
+      style: files.style.pattern,
+      options: {
+        includePaths: [ 'bower_components/scut/dist' ],
       }
+    },
+
+    autoprefixer: {
+      style: {
+        src: 'build/style/**/*.css'
+      },
+      options: { }
     },
 
     copy: {
-      script: {
-        expand: true,
-        cwd: 'src/',
-        dest: 'build/',
-        src: 'script/**/*.js',
-        ext: '.js'
-      },
-      resource: {
-        expand: true,
-        dest: 'build/',
-        src: 'asset/**',
-      },
+      script: files.script.pattern,
+      asset: files.asset.pattern,
+      options: { }
     },
 
     watch: {
-      script: {
-        files: ['src/**/*.js'],
-        tasks: ['copy:script'],
+      gruntfile: {
+        files: 'Gruntfile.js'
       },
-      resource: {
-        files: ['asset/**/*'],
-        tasks: ['copy:resource'],
+      script: {
+        files: [ files.script.path ],
+        tasks: [ 'script' ],
+      },
+      asset: {
+        files: [ files.asset.path ],
+        tasks: [ 'asset' ],
       },
       document: {
-        files: ['src/**/*.jade'],
-        tasks: ['jade'],
+        files: [ files.document.path ],
+        tasks: [ 'document' ],
       },
       style: {
-        files: ['src/**/*.styl'],
-        tasks: ['stylus'],
+        files: [ files.style.path ],
+        tasks: [ 'style' ],
       },
       options: {
         livereload: true
       }
     },
 
+    clean: [ BUILD_ROOT + '**/*' ],
+
     devserver: {
       server: {
         options: {
           port: 8020,
-          base: 'build/'
+          base: BUILD_ROOT
         }
       }
     }
 
   });
 
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-devserver');
+  grunt.loadNpmTasks('grunt-autoprefixer');
   grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-contrib-stylus');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-devserver');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', [ 'jshint', 'jade' , 'stylus', 'copy' ]);
+  grunt.registerTask('asset'    , [ 'copy:asset' ]);
+  grunt.registerTask('document' , [ 'jade:document' ]);
+  grunt.registerTask('style'    , [ 'sass:style', 'autoprefixer:style' ]);
+  grunt.registerTask('script'   , [ 'jshint:script', 'copy:script' ]);
+
+  grunt.registerTask('default', [
+                     'clean',
+                     'jshint:gruntfile',
+                     'asset',
+                     'document',
+                     'script',
+                     'style'
+  ]);
 
 };
 
