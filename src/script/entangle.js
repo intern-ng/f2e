45e357f9d0a928196901a5062c9765e24ae76abd
trinity <imandry.c@gmail.com>
2014-04-19 15:19:53 +0800

    entangle: cascading support (refactor)

--- src/script/entangle.js
+++ src/script/entangle.js
@@ -8,54 +8,75 @@ var entangle = (function () {
     this.runtime = {}; // chain-level runtime status object
   }
 
+  // @interface(converter>function) {{{
+
+  Entangle.prototype.call = function (ctx) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return this.apply(ctx, args);
+  };
+
+  Entangle.prototype.apply = function (ctx, args) {
+    return this.head.apply(this.contextof(this.head, this.runtime, ctx), args);
+  };
+
+  // }}}
+
+  Entangle.prototype.contextof = function (converter, runtime, context) {
+
+    var _this = this;
+
+    return {
+
+      // resolve - call next converter {{{
+      resolve: function () {
+        var next = converter.next;
+        if (next) {
+          // FIXME use arguments expansion here (efficiency)
+          fapply(next, _this.contextof(next, runtime, context), arguments);
+        } else {
+          // cascading resolve
+          fapply(context.resolve, context, arguments);
+        }
+        return this;
+      },
+      // }}} resolve
+
+      // failure - TODO issue conversion failure(s) {{{
+      failure: function () { return this; },
+      // }}} failure
+
+      // runtime - change the runtime container of next converter {{{
+      runtime: function (rt) {
+        if (!rt) {
+          return runtime;
+        } else {
+          runtime = rt;
+          return this;
+        }
+      },
+      // }}} runtime
+
+    };
+
+  };
+
   return _.extend(function () { return new Entangle(); }, {
 
     initiator: function (fu) {
 
       return function () {
-        var _this = this;
 
-        var converter = fu.apply(_this, Array.prototype.slice.call(arguments, 0));
+        var converter = fu.apply(this, Array.prototype.slice.call(arguments, 0));
 
         if (this.last) {
           this.last.next = converter;
         } else {
-          var contextof = function (converter, runtime) {
-            return {
-
-              // resolve - call next converter {{{
-              resolve: function () {
-                var args = Array.prototype.slice.call(arguments, 0);
-                var next = converter.next || _this.next; // cascading converter
-                // FIXME use arguments expansion here (efficiency)
-                if (next) next.apply(contextof(next, runtime), args);
-                return this;
-              },
-              // }}} resolve
-
-              // failure - TODO issue conversion failure(s) {{{
-              failure: function () { return this; },
-              // }}} failure
-
-              // runtime - change the runtime container of next converter {{{
-              runtime: function (rt) {
-                if (!rt) {
-                  return runtime;
-                } else {
-                  runtime = rt;
-                  return this;
-                }
-              },
-              // }}} runtime
-
-            };
-          };
-          _this.resolve = converter.bind(contextof(converter, _this.runtime));
+          this.head = converter;
         }
 
-        _this.last = converter;
+        this.last = converter;
 
-        return _this;
+        return this;
 
       };
 
