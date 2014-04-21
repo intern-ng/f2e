5ae036fd27f60ecb21603f207987c6157930bf66
trinity <imandry.c@gmail.com>
2014-04-21 11:52:30 +0800

    entangle.location: monitor window.location object

---
+++ src/script/entangle.ui.js
@@ -0,0 +1,41 @@
+/**
+ * Entangle - UI Toolkit
+ */
+
+entangle.extend({
+
+  /**
+   * @name location
+   * @desc window.location object
+   */
+  location: function () {
+
+    var converter = function () {
+      this.resolve(location);
+    };
+
+    // get location from window object
+    var location = {
+      host: window.location.host,
+      hostname: window.location.hostname,
+      protocol: window.location.protocol,
+      search: window.location.search,
+      href: window.location.href,
+      port: window.location.port,
+      pathname: window.location.pathname,
+      hash: window.location.hash,
+    };
+
+    var context = this.contextof(converter);
+
+    // listen for hash change event
+    $(window).on('hashchange', function () {
+      location.hash = window.location.hash;
+      context.resolve(location);
+    });
+
+    return converter;
+  },
+
+});
+
