567d580edfade90e2a2603eca5db26f4a7abb702
trinity <imandry.c@gmail.com>
2014-04-21 19:39:47 +0800

    script: navigation bar logic

--- src/script/navigation.js
+++ src/script/navigation.js
@@ -1,8 +1,51 @@
-////////////////
-// Navigation //
-////////////////
+////////////////////
+// Navigation Bar //
+////////////////////
 
-entangle().location().capture(function (pathname) {
-  $('.navbar-collapse a[href="' + pathname + '"]').parent().addClass('active');
+entangle().location().fork({
+
+  capture: entangle().capture(function (pathname) {
+    $('.navbar-collapse a[href="' + pathname + '"]').parent().addClass('active');
+  }),
+
+  user: entangle()
+  .capture().qs() // capture `search` parameter and pass to qs
+  .poll(eukit.io.HttpGet(function (qs) {
+    return '/u/' + qs.u;
+  }), 2000)
+  .fork({
+
+    data: entangle.capture('data'),
+
+    // set online status
+    online: entangle().capture(function (status) {
+      if (status == 200) {
+        return this.resolve({ set: 'online', reset: 'offline' });
+      } else {
+        return this.resolve({ set: 'offline', reset: 'online' });
+      }
+    })
+    .$apply('.navbar .navbar-control', {
+      addClass: 'set', removeClass: 'reset'
+    })
+
+  }).capture('data')
+
+}).capture('user').fork({
+
+  set_name: entangle()
+  .$apply('.navbar .text-profile-name', { text: 'name' }),
+
+  set_role: entangle()
+  .capture(function (role) {
+    return this.resolve({
+      set: role,
+      reset: _.without(['admin', 'teacher', 'student'], role).join(' ')
+    });
+  })
+  .$apply('.navbar .navbar-control', {
+      addClass: 'set', removeClass: 'reset'
+  })
+
 }).call();
 
