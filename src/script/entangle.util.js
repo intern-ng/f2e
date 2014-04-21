f932d7f93a88658fabdfb4c96c05b423e4070f04
trinity <imandry.c@gmail.com>
2014-04-21 16:47:59 +0800

    eukit: entangle utility kit

---
+++ src/script/entangle.util.js
@@ -0,0 +1,42 @@
+//////////////////////////
+// Entangle Utility Kit //
+//////////////////////////
+
+var eukit = {};
+
+_.extend(eukit, {
+
+  io: {}
+
+});
+
+////////
+// IO //
+////////
+
+_.extend(eukit.io, {
+
+  /**
+   * @name HttpGet
+   * @desc data juicer by performing http get request
+   */
+  HttpGet: function (url, data) {
+    return function () {
+      var _this = this;
+      var _url = (typeid(url) == 'function') ? url.apply(this, arguments) : url;
+      return ajax.get(_url, data, {
+        cache: false
+      }, ajax.handler({
+        200: function (data) {
+          return _this.resolve(data);
+        },
+        ___: function (data) {
+          return _this.failure(data);
+        }
+      }));
+    };
+  },
+
+});
+
+
