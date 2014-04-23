713764d533c1aee604778db3d402cb05b5bfa0bf
trinity <imandry.c@gmail.com>
2014-04-23 21:39:39 +0800

    entangle.app.extend: extend app threads

--- src/script/entangle.app.js
+++ src/script/entangle.app.js
@@ -13,6 +13,17 @@ entangle.Application = (function () {
 
     constructor: Application,
 
+    extend: function (obj) {
+      _.each(obj, function (v, k) {
+        if (this[k]) {
+          throw new ReferenceError ('duplicated declaration of `' + k + '`');
+        } else {
+          this[k] = v;
+        }
+      });
+      return this;
+    },
+
     route: function (obj) {
       _.each(obj, function (v, k) {
         this[k].fork(v);
