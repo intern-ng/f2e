997a600d0fd8f06adcf49dc7afd224fab5e76fe3
trinity <imandry.c@gmail.com>
2014-04-26 01:34:16 +0800

    faqedit.jade: toggle editor modes

--- src/script/magnet/faqedit.js
+++ src/script/magnet/faqedit.js
@@ -7,3 +7,30 @@ var options = {
 
 var pen = new Pen(options);
 
+
+// toggle editor mode
+document.querySelector('#mode').addEventListener('click', function() {
+  var text = this.textContent;
+
+  if(this.classList.contains('off')) {
+    this.classList.remove('off');
+    pen.rebuild();
+  } else {
+    this.classList.add('off');
+    pen.destroy();
+  }
+});
+
+// toggle editor mode
+document.querySelector('#hinted').addEventListener('click', function() {
+  var pen = document.querySelector('.pen');
+
+  if(pen.classList.contains('hinted')) {
+    pen.classList.remove('hinted');
+    this.classList.add('off');
+  } else {
+    pen.classList.add('hinted');
+    this.classList.remove('off');
+  }
+});
+
