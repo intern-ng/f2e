7fea2bc3931b5f292f87dba8a3f4756b9a284dd3
trinity <imandry.c@gmail.com>
2014-04-25 12:48:41 +0800

    entangle.poll: remove converter (refactor)

--- src/script/entangle.io.js
+++ src/script/entangle.io.js
@@ -15,50 +15,5 @@ entangle.extend({
     };
   }, // }}} data
 
-  /**
-   * @name Poll
-   * @desc Keep polling for data changes
-   * @param juicer {function} - function to juice data
-   * @param interval - time in milliseconds between each call to get data
-   */
-  poll: function (juicer, interval) { // {{{
-
-    var watch = (function () {
-      var timer;
-      return {
-        clear: function () { window.clearTimeout(timer); },
-        setup: function (func) {
-          watch.clear();
-          timer = window.setTimeout(func, interval);
-        }
-      };
-    })();
-
-    var status = 'stopped';
-    var args = [];
-
-    var converter = function () {
-      args = Array.prototype.slice.call(arguments, 0);
-      if (status != 'running') loop();
-    };
-
-    var loop = function () {
-      status = 'running'; watch.clear();
-      juicer.apply({
-        resolve: function () {
-          fapply(converter.resolve, converter, arguments);
-          status = 'stopped'; watch.setup(loop);
-        },
-        failure: function () {
-          /* TODO error handling */
-          status = 'stopped'; watch.setup(loop);
-        }
-      }, args);
-    };
-
-    return converter;
-
-  }, // }}} poll
-
 });
 
