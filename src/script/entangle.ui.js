49db5a026f1e2e15c115e910d9cbae015f0fa2cf
trinity <imandry.c@gmail.com>
2014-05-06 20:27:40 +0800

    entangle.ui: port all jquery functions to pipe format

--- src/script/entangle.ui.js
+++ src/script/entangle.ui.js
@@ -133,17 +133,40 @@ entangle.extend({
     };
   },
 
+  /**
+   * @name jQuery($)
+   * @desc create jQuery object
+   */
+  $: function () {
+    return function (el) {
+      return this.resolve($(el));
+    };
+  },
+
 });
 
 // Make jQuery shortcuts {{{
 
-_.each([ 'invoke' ], function (name) {
-  entangle.extend(pair(name + '$', function () {
-    var args = array(arguments);
-    var selector = args.shift();
-    return entangle[name].apply(null, [ $(selector) ].concat(args));
-  }));
-});
+entangle.extend(_(jQuery.fn).methods().transform(function (r, name) {
+  r['$' + name] = function () {
+    var capture = [ '$el', '___' ];
+    var dyparam = /{{([\s\S]+?)}}/;
+    var args = _.map(arguments, function (value) {
+      var match = dyparam.exec(value);
+      if (match) {
+        value = match[1];
+        capture.push(value);
+        return function (___) { return ___[value]; };
+      } else {
+        return function () { return value; };
+      }
+    });
+
+    return _.extend(function ($el, ___) {
+      return this.resolve(jQuery.fn[name].apply($el, _.map(args, function (get) { return get(___); })));
+    }, { capture: capture });
+  };
+}, {}).value());
 
 // }}}
 
