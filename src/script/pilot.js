be6ca23281d2b8b8dea19cd67563f5e9e0e60599
trinity <imandry.c@gmail.com>
2014-05-07 19:33:10 +0800

    pilot.js: refactor - use unnamed logic for non-reusable logic chain

--- src/script/pilot.js
+++ src/script/pilot.js
@@ -2,60 +2,82 @@
 // Navigation Bar //
 ////////////////////
 
+var $content = $('.content-container > .container');
+
 app.extend({
 
-  location: entangle().location(),
+  location: entangle()
+
+  .location(),      // provide window.location
 
   user_uri: entangle()
 
-  .pick( /* 'search' is auto-detected */ ).qs()
-  .pick(function (u) { this.resolve('/u/' + u); }, false)
-  .sponge(),
+  .pick().qs()      // pick `search` and resolve it use `qs`
+  .pick(false)      // do not force paremeter `u`
+  .string('u/{{u}}'),
 
   userdata: entangle()
 
-  .sponge(true).json('get').slot('raw').pick('data').sponge(),
-
-  userdata_poll: entangle()
-
-  .timeout(2000).data(),
-
-  navbar_set_path: entangle()
-
-  .pick().string('.navbar-collapse a[href="{{pathname}}"]').$().$parent('li').$addClass('active'),
-
-  navbar_set_line: entangle()
-
-  .pick('status').array().cases({ 200: 'online', ___: 'offline' })
-  .visibic$('.navbar-right [data-visibic], .navbar-left'),
-
-  $el_navbar_name: entangle().data('.navbar .data-holder.data-profile-name').$().pack('$el'),
-
-  navbar_set_name: entangle().sponge().pick().$text('{{name}}'),
-
-  navbar_set_role: entangle()
-
-  .pick('role')
-  .visibic$('.navbar-left [data-visibic]'),
+  .sponge(true)     // always trigger
+  .json('get')      // get json data from server
+  .slot('response')
+  .pick('data')     // pick data from response
+  .sponge(),        // cache the result & trigger limiter
 
 });
 
 app.dependency({
   user_uri: 'location',
   userdata: 'user_uri',
-  userdata_poll: 'userdata raw',
-  navbar_set_line: 'userdata raw',
-  navbar_set_path: 'location',
-  navbar_set_name: [ 'userdata', '$el_navbar_name' ],
-  navbar_set_role: 'userdata',
-});
-
-app.route({
-  userdata_poll: 'userdata',
 });
 
 // attach to entry point `main`
 app.route({
-  main: [ 'location', '$el_navbar_name' ]
+  init: [ 'location' ]
+});
+
+// connect timeout between `userdata response` and `userdata`
+app.route({
+  userdata: {
+    response: entangle().timeout(2000).data().fork(app.userdata)
+  }
+});
+
+app.route({
+
+  userdata: {
+
+    $: [
+
+      entangle()
+
+      .pick('role')
+      .visibic$('.navbar-left [data-visibic]'),
+
+      entangle()
+
+      .inject(
+        entangle().string('.navbar .data-holder.data-profile-name').$().pack('$el')
+      )
+      .pick().$text('{{name}}')
+
+    ],
+
+    response: entangle()
+
+    .pick('status').array()
+    .cases({ 200: 'online', ___: 'offline' })
+    .visibic$('.navbar-right [data-visibic], .navbar-left')
+
+  },
+
+  location: entangle()
+
+  .pick()
+  .string('.navbar-collapse a[href="{{pathname}}"]')
+  .$()
+  .$parent('li')
+  .$addClass('active'),
+
 });
 
