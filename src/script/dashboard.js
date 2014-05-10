579b5d243b2f76e0176abf558a3d8084d7a1cf5d
trinity <imandry.c@gmail.com>
2014-05-10 12:28:49 +0800

    announce edit on dashboard screen of administrator

--- src/script/dashboard.js
+++ src/script/dashboard.js
@@ -14,7 +14,7 @@ app.extend({
   home_set_dashboard: entangle()
 
   .pick('role')
-  .visibic$('.dashboard[data-visibic]')
+  .visibic$('.dashboard[data-visibic], .edit-announcement')
 
 });
 
@@ -22,4 +22,34 @@ app.dependency({
   home_set_profile: 'profile',
   home_set_dashboard: 'userdata'
 });
+
+$('.edit-announcement').click(function () {
+  var $el = $(this);
+
+  if ($el.hasClass('editing')) {
+
+    $el.removeClass('editing');
+    $el.text('编辑');
+
+    $('.announcement-editor').addClass('hidden');
+    $('.announcement-text').removeClass('invisible');
+
+    $('.announcement-text').text($('.announcement-editor').val());
+
+    localStorage.setItem('announcement', $('.announcement-editor').val());
+
+  } else {
+
+    $el.addClass('editing');
+    $el.text('保存');
+
+    $('.announcement-editor').removeClass('hidden');
+    $('.announcement-text').addClass('invisible');
+
+    $('.announcement-editor').val($('.announcement-text').text());
+
+  }
+});
+
+$('.announcement-text').text(localStorage.getItem('announcement') || '暂无公告');
 
